8acb1d2be9146125188d959b337ce33055de13d6
procodingclass@whitehatjr.com <procodingclass@whitehatjr.com>
2021-08-22 09:25:58 +0530

    Initial Comment

---
+++ sketch.js
@@ -0,0 +1,61 @@
+var trex, trexRunning;
+var ground, invisibleGround, groundImage;
+var cloud, cloudsGroup, cloudImage;
+
+function preload() {
+  trexRunning = loadAnimation("trex1.png", "trex2.png", "trex3.png");
+  groundImage = loadImage("ground2.png");
+  cloudImage = loadImage("cloud.png");
+}
+
+function setup() {
+  createCanvas(600, 200);
+
+  trex = createSprite(50, 160, 20, 50);
+  trex.addAnimation("running", trexRunning);
+  trex.scale = 0.5;
+
+  ground = createSprite(200, 180, 400, 20);
+  ground.addImage("ground", groundImage);
+  ground.x = ground.width / 2;
+  ground.velocityX = -4;
+
+  invisibleGround = createSprite(200, 190, 400, 10);
+  invisibleGround.visible = false;
+}
+
+function draw() {
+  background(220);
+
+  if (keyDown("space") && trex.y >= 100) {
+    trex.velocityY = -12;
+  }
+
+  trex.velocityY = trex.velocityY + 0.8;
+
+  if (ground.x < 0) {
+    ground.x = ground.width / 2;
+  }
+
+  trex.collide(invisibleGround);
+
+  //spawn the clouds
+  spawnClouds();
+
+  drawSprites();
+}
+
+function spawnClouds() {
+  //write code here to spawn the clouds
+  if (frameCount % 60 === 0) {
+    cloud = createSprite(600, 100, 40, 10);
+    cloud.addImage(cloudImage);
+    cloud.y = Math.round(random(10, 60));
+    cloud.scale = 0.4;
+    cloud.velocityX = -3;
+
+    //adjust the depth
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
+  }
+}
